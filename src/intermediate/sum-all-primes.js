ab1e14dae39e8241ea5ceb723376d8777ca0414b
Popovici, Marius <marius.popovici@abbott.com>
2020-01-03 13:45:20 -0500

    Refactored.

--- src/intermediate/sum-all-primes.js
+++ src/intermediate/sum-all-primes.js
@@ -8,23 +8,24 @@
  *
  * @param {Number} num
  */
-function sumPrimes(num) {
-  return genPrimes(num).reduce((acc, val) => acc + val);
-}
+const sumPrimes = num => genPrimes(num).reduce((acc, val) => acc + val);
 
 /**
  * Generates all prime numbers up to limit specified in *num*.
  * Implements the Sieve of Eratosthenes [algorithm](https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes)
+ * The sieve holds all non prime numbers i.e. all the multiples of existing primes. Any number that is
+ * not marked as non-prime (true) in the sieve is a prime
+ * .
  * @param {Number} num
  */
-function genPrimes(num) {
+const genPrimes = num => {
   let sieve = [];
   let primes = [];
   for (let i = 2; i <= num; i++) {
     if (!sieve[i]) {
       // if it's not in the sieve yet, it must be a prime number
       primes.push(i);
-      // mark all multiples of current number in the sieve
+      // mark all multiples of current number in the sieve as non prime.
       for (let n = 2; n * i <= num; n++) {
         sieve[n * i] = true;
       }
@@ -32,6 +33,6 @@ function genPrimes(num) {
   }
 
   return primes;
-}
+};
 
 export { sumPrimes, genPrimes };
